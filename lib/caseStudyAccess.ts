feb86d208a2f53019537a6e92730a5e9df36abf5
jay <jrambackup.1@gmail.com>
2026-04-25 02:19:45 +0530

    Unlock case studies on live site

--- lib/caseStudyAccess.ts
+++ lib/caseStudyAccess.ts
@@ -1,11 +1,1 @@
-export const areCaseStudiesUnlocked = (): boolean => {
-  if (import.meta.env.DEV) {
-    return true;
-  }
-
-  if (typeof window === 'undefined') {
-    return false;
-  }
-
-  return ['localhost', '127.0.0.1', '::1'].includes(window.location.hostname);
-};
+export const areCaseStudiesUnlocked = (): boolean => true;
